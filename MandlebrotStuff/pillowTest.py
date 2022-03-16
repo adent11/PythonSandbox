24cc8eeacd58ab55c70b56c78e2b72f807bf92cb
alldent22 <alldent22@gmail.com>
2022-03-15 22:57:39 -0400

    Start adding zoom functionality, slight refactor to drawing of set

---
+++ MandlebrotStuff/pillowTest.py
@@ -0,0 +1,5 @@
+from PIL import Image, ImageFilter
+
+im = Image.open('MandelbrotPics/100iterations7680x5120px.jpeg')
+
+im.show()
--- MandlebrotStuff/pygameMandlebrot.py
+++ MandlebrotStuff/pygameMandlebrot.py
@@ -1,7 +1,7 @@
 import pygame
 import math
 
-WIDTH, HEIGHT = 8640, 5760
+WIDTH, HEIGHT = 600, 400
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -12,9 +12,9 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Mandlebrot Set Drawer")
 WIN.fill((255, 255, 255))
 empty = pygame.Color(0, 0, 0, 0)
-RS, RE = -2, 1
-IS, IE = -1, 1
-maxIter = 100
+rS, rE = -2, 1
+iS, iE = -1, 1
+maxIter = 500
 
 
 def checkExit():
@@ -45,39 +45,62 @@ def nIters(p, n):
 
 
 def cplxToPos(p):
-    x = WIDTH * (p.real - RS) / (RE - RS)
-    y = HEIGHT * (p.imag - IS) / (IE - IS)
+    x = WIDTH * (p.real - rS) / (rE - rS)
+    y = HEIGHT * (p.imag - iS) / (iE - iS)
     return (x, y)
 
 
-for x in range(WIDTH):
-    print(x)
-    for y in range(HEIGHT):
-        point = complex(RS + (x / WIDTH) * (RE - RS),
-                        IS + (y / HEIGHT) * (IE - IS))
-        # print(point)
-        mP = int(mandlebrot(point))
-        color = (255 - mP * 255 / maxIter, 255 - mP * 255 / maxIter, 255 - mP * 255 / maxIter)
-        if color == (0, 0, 255):
-          color = (0, 0, 0)
-        mandlebrotSurface.set_at((x, y), color)
-        checkExit()
-WIN.blit(mandlebrotSurface, (0, 0))
-pygame.display.update()
-pygame.image.save(WIN, f"MandelbrotPics/{maxIter}iterations{WIDTH}x{HEIGHT}px.jpeg")
+def posToCplx(x, y):
+    real = rS + (x / WIDTH) * (rE - rS)
+    imag = iS + (y / HEIGHT) * (iE - iS)
+    return complex(real, imag)
 
+
+def zoomedBounds(cX, cY, zoom):
+    cplxC = posToCplx(cX, cY)
+    nRS = cplxC.real - (cplxC.real - rS) * zoom
+    nRE = cplxC.real + (rE - cplxC.real) * zoom
+    nIS = cplxC.imag - (cplxC.imag - iS) * zoom
+    nIE = cplxC.imag + (iE - cplxC.imag) * zoom
+    return nRS, nRE, nIS, nIE
+
+
+def drawMandelbrot():
+    for x in range(WIDTH):
+        print(x)
+        for y in range(HEIGHT):
+            point = complex(rS + (x / WIDTH) * (rE - rS),
+                            iS + (y / HEIGHT) * (iE - iS))
+            # print(point)
+            mP = int(mandlebrot(point))
+            color = (50 + mP * 205 / maxIter, 50 + mP *
+                     205 / maxIter, 255)
+            if color == (255, 255, 255):
+                color = (0, 0, 0)
+            mandlebrotSurface.set_at((x, y), color)
+            checkExit()
+    WIN.blit(mandlebrotSurface, (0, 0))
+    pygame.display.update()
+
+
+drawMandelbrot()
 nextFrameTime = pygame.time.get_ticks() + 10
 while True:
     checkExit()
     mX, mY = pygame.mouse.get_pos()
     print(f"Mouse X: {mX}  Mouse Y: {mY}")
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            rS, rE, iS, iE = zoomedBounds(mX, mY, .1)
+            drawMandelbrot()
+
     '''
     if pygame.time.get_ticks() > nextFrameTime:
         nextFrameTime = nextFrameTime + 10
         WIN.blit(mandlebrotSurface, (0, 0))
         if mX < WIDTH and mY < HEIGHT:
-            point = complex(RS + (mX / WIDTH) * (RE - RS),
-                            IS + (mY / HEIGHT) * (IE - IS))
+            point = complex(rS + (mX / WIDTH) * (rE - rS),
+                            iS + (mY / HEIGHT) * (iE - iS))
             n = 1
             while n < 16:
                 pygame.draw.line(WIN, (112, 169, 255), cplxToPos(
